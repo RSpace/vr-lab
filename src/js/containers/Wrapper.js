140976b77b72dcc54386422c039c233a01f7128c
Casper Fabricius <me@casperfabricius.com>
2016-01-10 11:19:20 +0100

    Messing aorund with positions and local testing

--- src/js/containers/Wrapper.js
+++ src/js/containers/Wrapper.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 //import DevTools from './DevTools'
-import 'aframe-core'
+import {registerComponent} from 'aframe-core'
+import {component} from 'aframe-text-component'
+registerComponent('text', component)
 import {Animation, Entity, Scene} from 'aframe-react'
 import { connect } from 'react-redux'
 
@@ -12,7 +14,21 @@ class Wrapper extends Component {
     return (
       <div>
         <Scene>
-          <Camera><Cursor/></Camera>
+          <Camera position="0 0 0"><Cursor/></Camera>
+          <Entity geometry={{primitive: 'sphere', radius: 9.5}}
+            material={{repeat: "50 25", shader: 'flat', src: 'url(assets/grid2.png)'}}
+            scale="1 1 -1"/>
+
+          <Entity text="text: DIRECTORS; size: 0.5" material="color: black" position="2.2 2.7 -5" rotation="0 -45 0" />
+          <a-entity geometry="primitive: plane; width: 5; height: 5;" position="4.75 1 -5" rotation="0 -45 0" material="color: black"></a-entity>
+
+          <Entity text="text: PRODUCERS; size: 0.2" material="color: black" position="4.75 -3 -5" rotation="0 -45 0" />
+          <a-entity geometry="primitive: plane; width: 2; height: 2;" position="4.75 -4 -5" rotation="0 -45 0" material="color: black"></a-entity>
+
+          <Entity text="text: ABOUT THE LAB; size: 0.2" material="color: black" position="-4.75 -4 -5" rotation="0 45 0" />
+          <a-entity geometry="primitive: plane; width: 2; height: 2;" position="-4.75 -3 -5" rotation="0 45 0" material="color: black"></a-entity>
+
+          <Entity text="text: EXHIBITIONS" material="color: black" position="-3.75 -3 -5" />
         </Scene>
       </div>
     )
