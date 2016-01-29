76d09f7c98d99526aba03927c06a7994567e7a8f
Casper Fabricius <me@casperfabricius.com>
2016-01-29 21:51:51 +0100

    Added preliminary back logos

--- src/js/containers/Wrapper.js
+++ src/js/containers/Wrapper.js
@@ -43,6 +43,8 @@ class Wrapper extends Component {
 
           <Entity position="0.15 -2 0" rotation="-90 0 0" onClick={openUrl.bind(this, 'https://itunes.apple.com/us/app/vrlab/id1045586626')} geometry="primitive: plane; width: 0.5; height: 0.25" material={{shader: 'flat', src: 'url(assets/gfx/app_store.png)'}} />
           <Entity position="-0.55 -2 0" rotation="-90 0 0" onClick={openUrl.bind(this, 'https://play.google.com/store/apps/details?id=com.makropol.vrlab')} geometry="primitive: plane; width: 0.5; height: 0.25" material={{shader: 'flat', src: 'url(assets/gfx/google_play.png)'}} />
+
+          <Entity position="0 0 1" rotation="0 180 0" geometry="primitive: plane; width: 2; height: 2" material={{shader: 'flat', src: 'url(assets/gfx/logoer.png)'}} />
         </Scene>
       </div>
     )
